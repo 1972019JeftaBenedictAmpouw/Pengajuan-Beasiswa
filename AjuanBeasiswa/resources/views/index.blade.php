94e7b71ad2ff5cf7201bd2352692e89fcd96fcde
Jefta1972019 <1972019@maranatha.ac.id>
2024-06-08 10:06:52 +0700

    Membuat blade dan controller, serta database dan model

---
+++ AjuanBeasiswa/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.index')
+
+@section('container')
+
+<div class="container py-3">
+    <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+    <div class="row">
+        <div class="col-lg-12">
+            <a href="/prodi" style="font-size: 50px;">Program Studi</a>
+        </div>
+        <div class="col-lg-12">
+            <a href="/fakultas" style="font-size: 50px;">Fakultas</a>
+        </div>
+        <div class="col-lg-12">
+            <a href="/mahasiswa" style="font-size: 50px;">Mahasiswa</a>
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
